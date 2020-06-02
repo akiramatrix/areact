b29393af515e65e43d2ef395543b49e6397bef73
akiramatrix <akira@g-mail.nsysu.edu.tw>
2020-06-02 20:15:45 +0800

    Updates

---
+++ precache-manifest.f62f708848dc600c1109a401bf6efad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed798b1f7516ed77bf58c4692e0934e2",
+    "url": "/akiramatrix/areact/index.html"
+  },
+  {
+    "revision": "7d6e1f4eecaa707d523a",
+    "url": "/akiramatrix/areact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65969750947a715475e0",
+    "url": "/akiramatrix/areact/static/js/2.2e06293d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/akiramatrix/areact/static/js/2.2e06293d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6e1f4eecaa707d523a",
+    "url": "/akiramatrix/areact/static/js/main.8dfdbfe9.chunk.js"
+  },
+  {
+    "revision": "b60a913e0e7a31f4789f",
+    "url": "/akiramatrix/areact/static/js/runtime-main.80410fdc.js"
+  }
+]);
