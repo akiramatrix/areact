e7e1f27342a5235ce2cb2d30969ceb3b18e4c163
akiramatrix <akira@g-mail.nsysu.edu.tw>
2020-06-02 19:27:51 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Welcome extends Component {
+  render() {
+    return <div>
+      <h1>Hello React</h1>
+      <h2>Mein Name ist Akira.</h2>
     </div>
-  );
+  }
 }
 
-export default App;
+class Main extends Component {
+  render() {
+    return <div>
+      <Welcome/>
+    </div>
+  }
+}
+
+class Hello extends Component{
+  render(){
+    return(
+      <div>
+        <p>Hello {this.props.title} </p>
+        <p> Ihr name ist {this.props.name} </p>
+      </div>
+    )
+
+  }
+}
+
+export default class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      myState: 'My first state',
+      saySomething: 'Hello World'
+    }
+  }
+  // method in this component
+  changeState = () =>{
+    this.setState({ myState:'Schönen Tag noch', saySomething: 'Danke!'})
+  }
+  render(){
+    return(
+      <div >
+        <Main />
+        <p> {this.state.myState} </p>
+        <p>{this.state.saySomething} </p>
+        <button onClick = {this.changeState}>Click me </button>
+        <button onClick = {()=>{this.setState({saySomething:'GOGO'})}}>Click me </button>
+        <button onClick={()=>{alert('hello')}}>Alert</button>
+        <Hello title="World!" name="Devil Cat man"/>        
+        <Hello title="React!" name = "IDK"/>
+      </div>
+    )
+  }
+}
+
+//export default Hello
+
